287ae1bcf65a321d43effafe3d5a353b4fb5aba3
wrewsama <andrewlzs512@gmail.com>
2023-08-20 17:35:30 +0800

    add footer

--- oshigoto/src/App.vue
+++ oshigoto/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppHeader from './components/AppHeader.vue'
 import AppBody from './components/AppBody.vue'
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
     <AppHeader />
     <AppBody />
+    <AppFooter />
 </template>
---
+++ oshigoto/src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer className="py-3 mt-auto bg-dark">
+			<div className="container px-4">
+				<p className="m0 text-center text-white">
+					&copy; Copyright 2023. Website by&nbsp;
+					<a className="text-decoration-none text-light font-weight-bold"
+					   href="https://github.com/wrewsama">
+						wrewsama
+					</a>
+				</p>
+			</div>
+		</footer>
+</template>
